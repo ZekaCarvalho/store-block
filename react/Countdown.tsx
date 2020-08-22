22bb0ae757a153a10533486f50108cdcd570cfa2
ZekaCarvalho <smart.artdigital@gmail.com>
2020-08-22 06:05:57 -0300

    Conecting backend and frontend

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
 import { useCssHandles } from 'vtex.css-handles'
-
+import { useQuery } from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
 
 
 interface CountdownProps {
@@ -20,15 +22,46 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = D
     seconds: '00'
   })
 
+  const { product: { linkText }, product } = useProduct()
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText
+    },
+    ssr: false
+  })
+
+
   const handles = useCssHandles(CSS_HANDLES);
 
-  tick(targetDate, setTime);
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    )
+  }
 
   return (
-   
-      <div className={`${handles.countdown} db tc`}>
-        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-      </div>
+
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
   )
 }
 
